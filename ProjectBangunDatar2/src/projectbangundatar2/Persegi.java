390fd16981ced93221d71af5964aaee1558412bc
DickyHendraP <71623032+DickyHendraP@users.noreply.github.com>
2021-10-10 03:35:30 +0700

    Create Persegi.java

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Persegi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author dickypmngks
+ */
+public class Persegi {
+    
+    //Hitung Luas
+    int hitungLuasPersegi(int sisi){
+        return (sisi * sisi);
+    }
+    
+    double hitungLuasPersegi(double sisi){
+        return (sisi * sisi);
+    }
+    
+    //Hitung Keliling
+    int hitungKelilingPersegi(int sisi){
+        return (sisi * 4);
+    }
+    
+    double hitungKelilingPersegi(double sisi){
+        return (sisi * 4);
+    }
+}
