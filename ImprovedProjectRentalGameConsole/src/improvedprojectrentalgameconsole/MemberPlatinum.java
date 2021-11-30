c66e43837228f28995e7ee559436d97f64cd9f1b
DickyHendraP <71623032+DickyHendraP@users.noreply.github.com>
2021-11-30 11:33:49 +0700

    Create MemberPlatinum.java

---
+++ ImprovedProjectRentalGameConsole/src/improvedprojectrentalgameconsole/MemberPlatinum.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package improvedprojectrentalgameconsole;
+
+/**
+ *
+ * @author dickypmngks
+ */
+public class MemberPlatinum extends MemberGold{
+    private int bonus;
+    
+    
+    MemberPlatinum() {
+        setBiayaRental(45000);
+        setPoin(10);
+        setDiskon(3);
+        setuangKembali(10000);
+    }
+    
+    protected int dapatBonus(int durasiRental) {
+        
+        bonus = durasiRental * 5000;
+        return bonus;
+    }
+}
