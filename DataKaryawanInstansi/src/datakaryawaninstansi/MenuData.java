66088f29b81be6b42f165e0f06cbdfaf3a0f1f52
DickyHendraP <71623032+DickyHendraP@users.noreply.github.com>
2021-12-26 22:18:31 +0700

    UAS_PBO

---
+++ DataKaryawanInstansi/src/datakaryawaninstansi/MenuData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datakaryawaninstansi;
+
+/**
+ *
+ * @author dickypmngks
+ */
+public abstract class MenuData implements Others{
+    
+
+    public void mnUtm(){
+        System.out.println("1. Tambah Data");
+        System.out.println("2. Hapus Data");
+        System.out.println("3. Cari Data");
+        System.out.println("4. Lihat Data");
+        System.out.println("5. Exit");
+    }
+}
---
+++ DataKaryawanInstansi/src/datakaryawaninstansi/SearchData.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datakaryawaninstansi;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author dickypmngks
+ */
+public class SearchData {
+    
+}
